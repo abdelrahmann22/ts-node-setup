4fe1a2008cdc6d207c702d6c0890fb5e53f1ddce
Abdelrahman <boudyali22@gmail.com>
2025-03-02 21:22:18 +0200

    Check

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,22 @@
-const username = "codeawy";
+import * as http from 'http'
 
-const hello = "hello";
+
+const server = http.createServer((req, res) => {
+	res.write(`
+		<html>
+			<head>
+				<title>Node.js App + TS</title>
+			</head>
+			<body>
+				<h1>Hello World!</h1>
+			</body>
+		</html>
+		`);
+	res.end();
+})
+
+const PORT: number = 5000;
+
+server.listen(PORT, ()=>{
+	console.log('Server Running!!!')
+})
